8c12e926890f70669791961b7b13b3e071d1c7b0
Natasha <natasha.bragg12@gmail.com>
2020-10-29 17:08:54 -0600

    updated project

--- JPATableTennisProject/src/main/java/com/skilldistillery/tabletennis/entities/User.java
+++ JPATableTennisProject/src/main/java/com/skilldistillery/tabletennis/entities/User.java
@@ -2,13 +2,14 @@ package com.skilldistillery.tabletennis.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class User {
 
 	@Id
-	@GeneratedValue	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private int id;
 	
 	private String username;
