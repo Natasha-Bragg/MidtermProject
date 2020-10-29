dad04dc0f41af2eda1a0c92b2e701a180d13490f
Ashley <yeleomg@Ashleys-MacBook-Air.local>
2020-10-29 16:56:40 -0600

    Stub User entity, test JPA mapping

--- JPATableTennisProject/src/main/java/com/skilldistillery/tabletennis/entities/User.java
+++ JPATableTennisProject/src/main/java/com/skilldistillery/tabletennis/entities/User.java
@@ -1,5 +1,48 @@
 package com.skilldistillery.tabletennis.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue	
+	private int id;
+	
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + "]";
+	}
+
+	public User(int id, String username) {
+		super();
+		this.id = id;
+		this.username = username;
+	}
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
 }
