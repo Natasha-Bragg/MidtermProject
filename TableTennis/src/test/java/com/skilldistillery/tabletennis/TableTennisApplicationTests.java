6f426d67d258f145826a3021c343d4cd404b636d
Natasha <natasha.bragg12@gmail.com>
2020-10-29 15:33:48 -0600

    Create Boot/MVC project

---
+++ TableTennis/src/test/java/com/skilldistillery/tabletennis/TableTennisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.tabletennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableTennisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
