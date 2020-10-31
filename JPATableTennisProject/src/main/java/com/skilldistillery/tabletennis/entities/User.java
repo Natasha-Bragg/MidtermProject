1266c9691a79b0031dde10e55a6f4dd3a9019134
jwolcott <jeanne.wolcott@gmail.com>
2020-10-31 12:25:58 -0600

    adding user entity and userTest

--- JPATableTennisProject/src/main/java/com/skilldistillery/tabletennis/entities/User.java
+++ JPATableTennisProject/src/main/java/com/skilldistillery/tabletennis/entities/User.java
@@ -1,25 +1,74 @@
 package com.skilldistillery.tabletennis.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 public class User {
-
+	//fields
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
-	private String username;
+	private String email;
+	private String password;
+	private int enabled;
+	private String role;
+	
+	@Column(name="first_name")
+	private String firstName;
 
+	@Column(name="last_name")
+	private String lastName;
+	
+	private int host;
+	private int travel;
+	
+	@Column(name="profile_image_url")
+	private String profileImageUrl;
+	
+	@OneToOne
+	@JoinColumn(name="address_id")
+	private Address address;
+	
+	@ManyToOne
+	@JoinColumn(name="skill_level_id")
+	private SkillLevel skillLevel;
+	
+	
+	//constructors
+	public User() {
+		super();
+	}
+	
+	public User(int id, String email, String password, int enabled, String role, String firstName, String lastName,
+			int host, int travel, String profileImageUrl) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+		this.enabled = enabled;
+		this.role = role;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.host = host;
+		this.travel = travel;
+		this.profileImageUrl = profileImageUrl;
+	}
+
+	//getters/setters
 	public String getUsername() {
-		return username;
+		return email;
 	}
 
 	public void setUsername(String username) {
-		this.username = username;
+		this.email = username;
 	}
 
 	public int getId() {
@@ -29,21 +78,122 @@ public class User {
 	public void setId(int id) {
 		this.id = id;
 	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(int enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getHost() {
+		return host;
+	}
+
+	public void setHost(int host) {
+		this.host = host;
+	}
+
+	public int getTravel() {
+		return travel;
+	}
+
+	public void setTravel(int travel) {
+		this.travel = travel;
+	}
+
+	public String getProfileImageUrl() {
+		return profileImageUrl;
+	}
+
+	public void setProfileImageUrl(String profileImageUrl) {
+		this.profileImageUrl = profileImageUrl;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public SkillLevel getSkillLevel() {
+		return skillLevel;
+	}
+
+	public void setSkillLevel(SkillLevel skillLevel) {
+		this.skillLevel = skillLevel;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", username=" + username + "]";
-	}
-
-	public User(int id, String username) {
-		super();
-		this.id = id;
-		this.username = username;
-	}
-
-	public User() {
-		super();
-		// TODO Auto-generated constructor stub
+		return "User [id=" + id + ", email=" + email + ", password=" + password + ", enabled=" + enabled + ", role="
+				+ role + ", firstName=" + firstName + ", lastName=" + lastName + ", host=" + host + ", travel=" + travel
+				+ ", profileImageUrl=" + profileImageUrl + ", address=" + address + ", skillLevel=" + skillLevel + "]";
 	}
 
 }
--- JPATableTennisProject/src/test/java/com/skilldistillery/tabletennis/entities/UserTest.java
+++ JPATableTennisProject/src/test/java/com/skilldistillery/tabletennis/entities/UserTest.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.tabletennis.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class UserTest {
@@ -41,9 +43,44 @@ class UserTest {
 	}
 
 	@Test
+	@DisplayName("testing user entity")
 	void test() {
+		/*
+		 select * from user where user.id = 1;
++----+------------------+----------+---------+-------+------------+-----------+------+--------+------------+----------------+----------------------------------------------------+
+| id | email            | password | enabled | role  | first_name | last_name | host | travel | address_id | skill_level_id | profile_image_url                                  |
++----+------------------+----------+---------+-------+------------+-----------+------+--------+------------+----------------+----------------------------------------------------+
+|  1 | ashley@email.com | admin    |       1 | admin | Ashley     | Davis     |    1 |      1 |          1 |              1 | https://en.wikipedia.org/wiki/File:Gambia_girl.jpg |
++----+------------------+----------+---------+-------+------------+-----------+------+--------+------------+----------------+----------------------------------------------------+
+		 */
 		assertNotNull(user);
-		assertEquals("admin", user.getUsername());
+		assertEquals("ashley@email.com", user.getEmail());
+		assertEquals("admin", user.getPassword());
+		assertEquals(1, user.getEnabled());
+		assertEquals("admin", user.getRole());
+		assertEquals("Ashley", user.getFirstName());
+		assertEquals("Davis", user.getLastName());
+		assertEquals(1, user.getHost());
+		assertEquals(1, user.getTravel());
+		assertEquals("https://en.wikipedia.org/wiki/File:Gambia_girl.jpg", user.getProfileImageUrl());
+	}
+	
+	@Test
+	@DisplayName("testing user to address")
+	void test1() {
+		/*
+		 select address.street, address.city, address.state, address.zip, user.first_name FROM user JOIN address ON address.id = user.address_id where user.id = 1;
++------------------+-------------+-------+-------+------------+
+| street           | city        | state | zip   | first_name |
++------------------+-------------+-------+-------+------------+
+| 123 Apple Street | New Orleans | LA    | 70094 | Ashley     |
++------------------+-------------+-------+-------+------------+
+		 */
+		Address address = user.getAddress();
+		assertEquals("123 Apple Street", address.getStreet());
+		assertEquals("New Orleans", address.getCity());
+		assertEquals("LA", address.getState());
+		assertEquals("70094", address.getZip());
 	}
 
 }
